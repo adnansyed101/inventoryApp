c352e4cc3a4bce18096e0c4207792fbd88b77252
Md Adnan <sirgeant10@gmail.com>
2023-12-15 15:06:36 +0600

    increase max length for desc

--- models/product.js
+++ models/product.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema({
   name: { type: String, required: true, maxLength: 100 },
-  description: { type: String, maxLength: 300 },
+  description: { type: String, maxLength: 1000 },
   category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
   price: { type: Number },
   inStock: { type: Number },
