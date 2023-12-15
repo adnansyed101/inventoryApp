4d08eb01b6d0d513f84e6331b662bb1ad9f1da25
Md Adnan <sirgeant10@gmail.com>
2023-12-15 12:28:28 +0600

    remove description

--- models/category.js
+++ models/category.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
   name: { type: String, require: true, maxLength: 100 },
-  description: { type: String, maxLength: 300 },
 });
 
 CategorySchema.virtual("url").get(function () {
