16cad250e96142e8312e43df734e6108482ea090
Md Adnan <sirgeant10@gmail.com>
2023-12-15 12:31:06 +0600

    fix spelling

--- models/product.js
+++ models/product.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema({
-  name: { type: String, require: true, maxLength: 100 },
+  name: { type: String, required: true, maxLength: 100 },
   description: { type: String, maxLength: 300 },
   category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
   price: { type: Number },
