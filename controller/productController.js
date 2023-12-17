c834de940b25b34fb90e8a6931cc56db384f6b83
Md Adnan <sirgeant10@gmail.com>
2023-12-17 15:09:27 +0600

    complete index route
    Now the index route will show the records of products and categories

--- controller/productController.js
+++ controller/productController.js
@@ -1,46 +1,57 @@
 const Product = require("../models/product");
+const Category = require("../models/category");
 const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Site Home Page");
+  // Get Details of Products and Categories in Parallel
+  const [numProducts, numCategories] = await Promise.all([
+    Product.countDocuments({}).exec(),
+    Category.countDocuments({}).exec(),
+  ]);
+
+  res.render("index", {
+    title: "Inventory App Home",
+    product_count: numProducts,
+    category_count: numCategories,
   });
-  
-  // Display list of all products.
-  exports.product_list = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: product list");
-  });
-  
-  // Display detail page for a specific product.
-  exports.product_detail = asyncHandler(async (req, res, next) => {
-    res.send(`NOT IMPLEMENTED: product detail: ${req.params.id}`);
-  });
-  
-  // Display product create form on GET.
-  exports.product_create_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: product create GET");
-  });
-  
-  // Handle product create on POST.
-  exports.product_create_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: product create POST");
-  });
-  
-  // Display product delete form on GET.
-  exports.product_delete_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: product delete GET");
-  });
-  
-  // Handle product delete on POST.
-  exports.product_delete_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: product delete POST");
-  });
-  
-  // Display product update form on GET.
-  exports.product_update_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: product update GET");
-  });
-  
-  // Handle product update on POST.
-  exports.product_update_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: product update POST");
-  });
+});
+
+// Display list of all products.
+exports.product_list = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: product list");
+});
+
+// Display detail page for a specific product.
+exports.product_detail = asyncHandler(async (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: product detail: ${req.params.id}`);
+});
+
+// Display product create form on GET.
+exports.product_create_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: product create GET");
+});
+
+// Handle product create on POST.
+exports.product_create_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: product create POST");
+});
+
+// Display product delete form on GET.
+exports.product_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: product delete GET");
+});
+
+// Handle product delete on POST.
+exports.product_delete_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: product delete POST");
+});
+
+// Display product update form on GET.
+exports.product_update_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: product update GET");
+});
+
+// Handle product update on POST.
+exports.product_update_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: product update POST");
+});
