a648918c27e33e25eebbf8bd931a85db47af4cee
Md Adnan <sirgeant10@gmail.com>
2024-01-02 13:31:35 +0600

    fix route
    the route was going to products. But should have been going to category.

--- models/category.js
+++ models/category.js
@@ -8,7 +8,7 @@ const CategorySchema = new Schema({
 
 CategorySchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
-  return `/catalog/product/${this._id}`;
+  return `/catalog/category/${this._id}`;
 });
 
 module.exports = mongoose.model("Category", CategorySchema);
