ee7cc40181e987ff07ecadf080d4df1f464b5684
Adnan <sirgeant10@gmail.com>
2024-09-26 16:04:46 +0600

    Fixed error handling

--- controllers/publisherControllers.js
+++ controllers/publisherControllers.js
@@ -1,29 +1,30 @@
 const { body, validationResult } = require("express-validator");
 const db = require("../db/queries");
 
-const alphaErr = "Must only contain letters.";
-const lengthErr = "Must be between 1 and 10 characters.";
+const lengthErr = "Must be between 2 and 10 characters.";
 
 const validatePublisherName = [
   body("publisherName")
     .trim()
-    .isAlpha()
-    .withMessage(`Publisher name ${alphaErr}`)
-    .isLength({ min: 1, max: 50 })
+    .isLength({ min: 2, max: 50 })
     .withMessage(`Publisher name ${lengthErr}`),
 ];
 
-function publisherCreateGet(req, res) {
-  res.render("publisher", { title: "Create Publisher" });
+async function publisherCreateGet(req, res) {
+  const publishers = await db.getPublisherName();
+  console.log(publishers);
+
+  res.render("publisher", { title: "Create Publisher", publishers });
 }
 
 const publisherCreatePost = [
   validatePublisherName,
   async (req, res) => {
     const errors = validationResult(req);
+    console.log(errors);
     if (!errors.isEmpty()) {
-      return res.status(400).render("createUser", {
-        title: "Create Publisher",
+      return res.status(400).render("404", {
+        title: "404 Something went wrong",
         errors: errors.array(),
       });
     }
